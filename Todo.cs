7ffb9bded632c872406f178bc9de2f65f7a43166
Mike Kistler (from Dev Box) <mikekistler@microsoft.com>
2025-04-17 21:03:16 -0500

    Add Todo model

---
+++ Todo.cs
@@ -0,0 +1,12 @@
+﻿namespace ScaffoldCheck;
+
+public class Todo
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public bool IsComplete { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime? CompletedAt { get; set; }
+    public string? Description { get; set; }
+    public string? Tags { get; set; }
+}
